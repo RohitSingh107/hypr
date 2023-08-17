7b1d57196da322b1c72b17706dff7047d6f9c2c5
Rohit <RohitSinghEmail@protonmail.com>
2023-08-17 21:07:59 +0530

    uppdate

---
+++ scripts/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export QT_QPA_PLATFORM=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=sway
+export USE_WAYLAND_GRIM=true
+flameshot gui
